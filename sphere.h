ef45f4785bc466dd36076b28a2a517a779d69da5
GaelleMarais <marais.gaelle@gmail.com>
2019-09-18 12:55:22 +0100

    sphere.h: Add intersection_max

--- sphere.h
+++ sphere.h
@@ -39,3 +39,31 @@ float intersection(Ray ray, Sphere sphere){
     //printf("%f", solution);
     return solution;
 }
+
+float intersection_max(Ray ray, Sphere sphere){
+    float a = 1;
+    float b = 2*(dot(ray.P, ray.D) - dot(sphere.C, ray.D));
+    float c = dot(ray.P,ray.P)
+             +dot(sphere.C,sphere.C)
+             -(dot(sphere.C,ray.P)*2)
+             -(sphere.R*sphere.R);
+
+    float delta = (b*b) - (4*a*c);
+    float solution;
+
+    if(delta>0){
+        float s1 = (-b - std::sqrt(delta))/ (2*a);
+        float s2 = (-b + std::sqrt(delta))/ (2*a);
+        if (s1>0)
+            solution = s2;
+        else if (s2>0)
+            solution = s2;
+        else 
+            return 0;
+    }else{
+        return 0;
+    }
+    //DEBUG
+    //printf("%f", solution);
+    return solution;
+}

1c4395bde8a3d998eb39984c2e3c900168e9dda7
GaelleMarais <marais.gaelle@gmail.com>
2019-09-24 11:24:40 +0100

    cube.h: Add cube struct

---
+++ cube.h
@@ -0,0 +1,19 @@
+#include "vec3.h"
+
+struct Cube{
+    Vec3<float> P;
+    float size;
+};
+
+Vec3<float> randomPoint(Cube cube){
+    float randx =  (static_cast <float> (rand()) / static_cast <float> (RAND_MAX))*cube.size + cube.P.x;
+    float randy =  (static_cast <float> (rand()) / static_cast <float> (RAND_MAX))*cube.size + cube.P.y;
+    float randz =  (static_cast <float> (rand()) / static_cast <float> (RAND_MAX))*cube.size + cube.P.z;
+    // float randx = cube.P.x + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(cube.size-cube.P.x)));
+    // float randy = cube.P.y + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(cube.size-cube.P.y)));
+    // float randz = cube.P.z + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(cube.size-cube.P.z)));
+    Vec3<float> vec = {randx,randy,randz};
+    // DEBUG
+    // printf("randx : %f     randy : %f     randz : %f\n", randx, randy, randz);
+    return vec;
+}

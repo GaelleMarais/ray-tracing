3c2c110ed53272421215cf25ef2a6df2ca57b86e
GaelleMarais <marais.gaelle@gmail.com>
2019-09-30 16:32:23 +0100

    vec3.h : Add more operators

--- vec3.h
+++ vec3.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <cmath>
+#include <iostream>
 
 template<typename T>
 struct Vec3 {
@@ -15,6 +16,27 @@ template<typename T>
 Vec3<T> operator*(const Vec3<T>& a, const Vec3<T>& b) {
 	return Vec3<T>{ a.x * b.x, a.y * b.y, a.z * b.z };
 };
+
+template<typename T>
+Vec3<T> operator*(const Vec3<T>& a, const float& scalar) {
+	return Vec3<T>{ a.x * scalar, a.y * scalar, a.z * scalar };
+};
+
+template<typename T>
+Vec3<T> operator*(const float& scalar, const Vec3<T>& a) {
+	return a * scalar;
+}
+
+template<typename T>
+Vec3<T> operator/(const Vec3<T>& a, const float& scalar) {
+	return Vec3<T>{ a.x / scalar, a.y / scalar, a.z / scalar };
+};
+
+template<typename T>
+Vec3<T> operator/(const float& scalar, const Vec3<T>& a) {
+	return a / scalar;
+}
+
 
 template<typename T>
 Vec3<T> operator/(const Vec3<T>& a, const Vec3<T>& b) {
@@ -39,4 +61,18 @@ float norm(const Vec3<T>& v) {
 template<typename T>
 float dot(const Vec3<T>& a, const Vec3<T>&b) {
 	return a.x* b.x + a.y * b.y + a.z * b.z;	
+}
+
+template<typename T>
+Vec3<T> normalise(const Vec3<T>& a){
+	float n = norm(a);
+	if (!n) return {0, 0, 0};
+	return {a.x/n, a.y/n, a.z/n};
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& stream, const Vec3<T>& v)
+{
+	stream << "(" << v.x << ", " << v.y << ", " << v.z << ")";
+	return stream;
 }

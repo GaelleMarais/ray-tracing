9ca798a2f8a7b4b79bc09275f7fcd4ca0ed3c1cc
GaelleMarais <marais.gaelle@gmail.com>
2019-10-01 16:01:33 +0100

    sphere.h: Remove useless function intersection_max

--- sphere.h
+++ sphere.h
@@ -5,6 +5,7 @@ struct Sphere{
     Vec3<float> C; //center
     int R;  //radius
     Vec3<float> color;
+    int reflection;
 };
 
 bool operator==(Sphere a, Sphere b){
@@ -53,32 +54,4 @@ float nearest_intersection(Ray ray, Sphere spheres[], int nb_sphere, Sphere &sph
         }
     }
     return min_f;
-}
-
-float intersection_max(Ray ray, Sphere sphere){
-    float a = 1;
-    float b = 2*(dot(ray.P, ray.D) - dot(sphere.C, ray.D));
-    float c = dot(ray.P,ray.P)
-             +dot(sphere.C,sphere.C)
-             -(dot(sphere.C,ray.P)*2)
-             -(sphere.R*sphere.R);
-
-    float delta = (b*b) - (4*a*c);
-    float solution;
-
-    if(delta>0){
-        float s1 = (-b - std::sqrt(delta))/ (2*a);
-        float s2 = (-b + std::sqrt(delta))/ (2*a);
-        if (s1>0)
-            solution = s2;
-        else if (s2>0)
-            solution = s2;
-        else 
-            return 0;
-    }else{
-        return 0;
-    }
-    //DEBUG
-    //printf("%f", solution);
-    return solution;
 }

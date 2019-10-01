c5d8a094c1b7b728e24120119669cbc3f4f23ecb
GaelleMarais <marais.gaelle@gmail.com>
2019-10-01 10:45:27 +0100

    main.cpp: Add perspective and walls

--- main.cpp
+++ main.cpp
@@ -24,14 +24,12 @@ float clamp(float min, float max, float value)
     return value;
 }
 
-//t e [0; 1]
 float lerp(float start, float end, float t)
 {
     return start + t * (end - start);
 }
 
-Vec3<float> get_point_light_illumination(const Light& light, const Vec3<float> hit_point, const Vec3<float>& normal, Scene scene)
-{
+Vec3<float> get_point_light_illumination(const Light& light, const Vec3<float> hit_point, const Vec3<float>& normal, Scene scene){
     Vec3<float> color = {0, 0, 0};
     // Compute point light color
 
@@ -42,11 +40,10 @@ Vec3<float> get_point_light_illumination(const Light& light, const Vec3<float> h
     float dist_to_light = norm(light.position - hit_point);
 
     Sphere obstacle;
-    // int obs = 0;
     float f_obstacle = nearest_intersection(sphere_to_light, scene.spheres, scene.nb_spheres, obstacle);
 
     if (f_obstacle == -1 || f_obstacle >= dist_to_light){
-        // sphere is in light
+        // case where the sphere is in light
         float angle = dot(sphere_to_light.D, normal);
         float intensity = light.intensity * angle / (dist_to_light * dist_to_light);
         if (intensity < 0) intensity = 0;
@@ -56,19 +53,24 @@ Vec3<float> get_point_light_illumination(const Light& light, const Vec3<float> h
     return color;
 }
 
-void draw_sphere_ppm(PPM ppm, Sphere s, Scene scene){
+Ray get_ray(const Vec3<float> pixel, const Vec3<float> camera){
+    Ray ray;
+    ray.P = camera;
+    ray.D = normalise(pixel - camera);
+    return ray;
+}
 
-
+void draw_scene_ppm(PPM ppm, Scene scene){
 
     #pragma omp parallel for    
     for(int i = 0; i < ppm.width; i++){
         for(int j = 0; j < ppm.height; j++){
 
-            Ray r;
-            r.P = {(float)i,(float)j,0};
-            r.D = {0,0,1};
+            Vec3<float> pixel = {(float)i, (float)j, 0};
+            Ray r = get_ray(pixel, scene.camera);
 
-            float f = intersection(r,s);             
+            Sphere s;
+            float f = nearest_intersection(r, scene.spheres, scene.nb_spheres, s);  
 
             if(f>0){ // case where the sphere is in the screen
 
@@ -76,7 +78,6 @@ void draw_sphere_ppm(PPM ppm, Sphere s, Scene scene){
                 Vec3<float> normal = normalise(hit_point - s.C);
                 Vec3<float> illumination = {0, 0, 0};
                 
-                //Can abstract to multiple light_cubes easily now ! :)
                 Vec3<float> light_cube_illumination = {0, 0, 0};
 
                 std::random_device random_device;
@@ -129,53 +130,86 @@ int main(int argc, char* argv[])
     sscanf("imgs/scene.ppm","%s", filename);
     printf("Writing %s ... ", filename);
 
-    // WHITE SPHERE
-    Sphere s3;
-    s3.C = {300, 300, 1500};
-    s3.R = 300;
-    s3.color={1,1,1};
-    scene.spheres[0]=s3;
-
-    // GREEN SPHERE
+    // WHITE BACKGROUND
     Sphere s1;
-    s1.C = {150, 250, 1000};
-    s1.R = 80;
-    s1.color={0,1,1};
-    scene.spheres[1]=s1;
+    s1.C = {300, 300, 20600};
+    s1.R = 20000;
+    s1.color={1,1,1};
+    scene.spheres[0]=s1;
 
-    // YELLOW SPHERE
+    // WHITE CEILING
     Sphere s2;
-    s2.C = {400, 300, 1000};
-    s2.R = 70;
-    s2.color={1,1,0};
-    scene.spheres[2]=s2;
+    s2.C = {300, -20000, 300};
+    s2.R = 20000;
+    s2.color={1,1,1};
+    scene.spheres[1]=s2;
 
-    scene.nb_spheres=3;
+    // WHITE FLOOR
+    Sphere s3;
+    s3.C = {300, 20600, 300};
+    s3.R = 20000;
+    s3.color={1,1,1};
+    scene.spheres[2]=s3;
+
+    // WHITE LEFT WALL
+    Sphere s4;
+    s4.C = {-20000, 300, 300};
+    s4.R = 20000;
+    s4.color={1,1,1};
+    scene.spheres[3]=s4;
+
+    // WHITE RIGHT WALL
+    Sphere s5;
+    s5.C = {20600, 300, 300};
+    s5.R = 20000;
+    s5.color={1,1,1};
+    scene.spheres[4]=s5;
+
+    // LIGHT BLUE SPHERE
+    Sphere s6;
+    s6.C = {400, 400, 400};
+    s6.R = 120;
+    s6.color={0,1,1};
+    scene.spheres[5]=s6;
+
+    // BLUE SPHERE
+    Sphere s7;
+    s7.C = {150, 250, 350};
+    s7.R = 80;
+    s7.color={0,0.3,1};
+    scene.spheres[6]=s7;
+
+
+
+
+    scene.nb_spheres=7;
+
+
 
     // YELLOW LIGHT
     LightCube l1;
     l1.color = {1, 1, 0};
     l1.intensity = light_intensity;
-    l1.position = {300, 600, 0};
-    l1.size = 100;
+    l1.position = {300, 600, 300};
+    l1.size = 300;
     scene.light_cubes[0] = l1;
 
-    // RED LIGHT
+    // WHITE LIGHT
     LightCube l2;
-    l2.color = {1, 0, 0};
-    l2.intensity = light_intensity *2;
-    l2.position = {300, 300, 0};
-    l2.size = 50;
+    l2.color = {1, 1, 1};
+    l2.intensity = light_intensity;
+    l2.position = {300, 100, 300};
+    l2.size = 20;
     scene.light_cubes[1] = l2;
     
     scene.nb_lights = 2;
 
+    scene.camera = {300,300,-2500};
+
 
     PPM ppm = easyppm_create(image_width, image_height, IMAGETYPE_PPM);
 
-    for(int i=0; i<scene.nb_spheres; i++){
-        draw_sphere_ppm(ppm, scene.spheres[i], scene);
-    }
+    draw_scene_ppm(ppm, scene);
 
     easyppm_write(&ppm, filename);
     printf("OK\n");

023500cb6a8cd1ffb7384b1ea176d38543a3f948
GaelleMarais <marais.gaelle@gmail.com>
2019-10-03 14:31:54 +0100

    sphere.h: Rename functions

--- sphere.h
+++ sphere.h
@@ -3,9 +3,8 @@
 
 struct Sphere{
     Vec3<float> C; //center
-    int R;  //radius
+    float R;  //radius
     Vec3<float> color;
-    int reflection;
 };
 
 bool operator==(Sphere a, Sphere b){
@@ -15,7 +14,7 @@ bool operator==(Sphere a, Sphere b){
 
 // Intersection between the ray and the sphere's surface
 // Indicates where to draw the sphere in the screen
-float intersection(Ray ray, Sphere sphere){
+float intersection_sphere(Ray ray, Sphere sphere){
     float a = 1;
     float b = 2*(dot(ray.P, ray.D) - dot(sphere.C, ray.D));
     float c = dot(ray.P,ray.P)
@@ -43,11 +42,11 @@ float intersection(Ray ray, Sphere sphere){
     return solution;
 }
 
-float nearest_intersection(Ray ray, Sphere spheres[], int nb_sphere, Sphere &sphere){
+float nearest_intersection_sphere(Ray ray, Sphere spheres[], int nb_sphere, Sphere &sphere){
     float min_f = -1;
     for (int i=0; i<nb_sphere; i++){
         Sphere s = spheres[i];
-        float f = intersection(ray, s);
+        float f = intersection_sphere(ray, s);
         if (f > 0 && (min_f == -1 || f < min_f)){
             sphere=s;
             min_f=f;

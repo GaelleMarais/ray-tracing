8f941e71411fc020a6d38e5807ac1a1c4353b45d
GaelleMarais <marais.gaelle@gmail.com>
2019-10-08 08:57:07 +0100

    parser.h: Add different delim

--- parser.h
+++ parser.h
@@ -6,9 +6,10 @@
 #include <fstream>
 #include <string>
 
+
 using namespace std;
 
-Triangle* read_file(string filename, int size){
+void read_file(string filename, int size,Vec3<float> pos, Triangle triangles[]){
 
   string buff;
   ifstream file (filename);
@@ -18,54 +19,59 @@ Triangle* read_file(string filename, int size){
     getline(file, buff); // OFF
 
     int nb_vertices, nb_triangles;
-    char delim=' ';
+    char delim_space=' ';
+    char delim_endline='\n';
 
-    getline(file,buff, delim); 
+    getline(file,buff, delim_space); 
     nb_vertices = stoi(buff);
-    getline(file,buff, delim); 
+    getline(file,buff, delim_space); 
     nb_triangles = stoi(buff);
-    getline(file,buff, delim);  // nb_edges
+    getline(file,buff, delim_endline);  // nb_edges
 
 
     Vec3<float> vertices[nb_vertices];
-    Triangle* triangles = (Triangle *) malloc (sizeof (Triangle) * nb_triangles);
 
     for(int i = 0; i < nb_vertices; i++){
 
-        getline(file, buff, delim);
-        float x = stof(buff) * (float) size;
-        getline(file, buff, delim);
-        float y = stof(buff) * (float) size;
-        getline(file, buff, delim);
-        float z = stof(buff) * (float) size;
+      getline(file, buff, delim_space);
+      float x = stof(buff) * (float) size + pos.x;
+      getline(file, buff, delim_space);
+      float y = stof(buff) * (float) size + pos.y;
+      getline(file, buff, delim_endline);
+      float z = stof(buff) * (float) size + pos.z;
 
-        vertices[i] = {x, y, z};
+      vertices[i] = {x, y, z};  
+      // DEBUG     
+      // std::cout << vertices[i] << std::endl; 
 
     }
 
     for( int j = 0; j < nb_triangles; j++){
 
-        Triangle triangle;
+      Triangle triangle;
 
-        getline(file, buff, delim); // 3
+      getline(file, buff, delim_space); // 3
 
-        getline(file, buff, delim);
-        triangle.a = vertices[stoi(buff)];
-        getline(file, buff, delim);
-        triangle.b = vertices[stoi(buff)];
-        getline(file, buff, delim);
-        triangle.c = vertices[stoi(buff)];
+      getline(file, buff, delim_space);
+      triangle.a = vertices[stoi(buff)];
+      getline(file, buff, delim_space);
+      triangle.b = vertices[stoi(buff)];
+      getline(file, buff, delim_endline);
+      triangle.c = vertices[stoi(buff)];
 
-        triangle.color = {1, 0, 0};
+      triangle.color = {1, 0, 0};
 
-        triangles[j] = triangle;
-
+      triangles[j] = triangle;
 
      }
     file.close();
 
-    return triangles;
   }
 
-  return 0;
+}
+
+string read_buff_str(ifstream file, string buff, char delim){
+  buff = "";
+  getline(file, buff, delim);
+  return buff;
 }
